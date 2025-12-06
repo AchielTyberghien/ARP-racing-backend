024043e66fddf4989ba7c1be9be8299449e4a5f1
Achiel Tyberghien <achiel.tyberghien@student.vives.be>
2025-12-06 13:46:13 +0100

    carousel endpoint

---
+++ routes/carousel.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const encodedPath = encodeURIComponent("/Arp Racing/Carousel/");
+
+    const url = `${process.env.IMAGEKIT_BASE}?path=${encodedPath}`;
+
+    const authString = Buffer
+      .from(`${process.env.IMAGEKIT_PRIVATE_KEY}:`)
+      .toString("base64");
+
+    const response = await axios.get(url, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Basic ${authString}`
+      }
+    });
+
+    const fileNames = response.data.map(file => file.name);
+
+    res.json({
+      success: true,
+      files: fileNames,
+    });
+
+  } catch (error) {
+    const apiResponse = error.response ? error.response.data : null;
+
+    res.status(500).json({
+      success: false,
+      message: "Error fetching ImageKit data",
+      error: error.message,
+      apiResponse,
+    });
+  }
+});
+
+module.exports = router;
