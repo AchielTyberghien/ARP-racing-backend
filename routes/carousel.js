b24f96079a56f547744df68cc14f66b0c5436332
Achiel Tyberghien <achiel.tyberghien@student.vives.be>
2026-01-03 14:44:47 +0100

    white space

--- routes/carousel.js
+++ routes/carousel.js
@@ -21,6 +21,7 @@ router.get("/", async (req, res) => {
 
     const pictureList = response.data.map(file => ({ name: file.name, description: file.description }));
 
+    
     res.json({
       success: true,
       pictures: pictureList,
